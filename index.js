6cbe9981436c18f6eb7cfab5a869e525e586615c
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2023-11-11 12:36:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.ceil(Math.random()*6);
+var randomNumber2 = Math.ceil(Math.random()*6);
+
+
+document.querySelector(".dice .img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelector(".dice .img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
+
+
+
+if (randomNumber1 > randomNumber2) {
+   document.querySelector("h1").innerHTML= "🚩Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 wins!🚩";
+}
+else {
+    document.querySelector("h1").textContent = "🚩Draw!🚩";
+}
+
