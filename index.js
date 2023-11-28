dcc33f2342deceff4e0e167cf055d900420e0c63
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2023-11-28 01:56:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var randomNumber2 = Math.ceil(Math.random()*6);
 document.querySelector(".dice .img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
 document.querySelector(".dice .img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
 
-
-
 if (randomNumber1 > randomNumber2) {
    document.querySelector("h1").innerHTML= "🚩Player 1 wins!";
 }
